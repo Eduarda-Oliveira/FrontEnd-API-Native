8104a6aef6f9eb51b6e9af8afbc3f52fb0ff38ee
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-05-03 14:32:55 -0300

    organizando o menu

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Login } from './src/screens/login';
+import { Search } from './src/screens/search';
+import { Index } from './src/screens/index';
 import Icon from 'react-native-vector-icons/Feather';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -12,6 +14,9 @@ const screenOptions = (route, color) => {
   switch (route.name) {
     case 'Login':
       iconName = 'users';
+      break;
+    case 'Search':
+      iconName = 'search';
       break;
     default:
       break;
@@ -31,6 +36,8 @@ export default function App() {
         tabBarIcon: ({color}) => screenOptions(route, color),
       })}>
       <tab.Screen name="Login" component={Login}/>
+      <tab.Screen name="Search" component={Search}/>
+      <tab.Screen name="Index" component={Index}/>
     </tab.Navigator>
     </NavigationContainer>
   );
