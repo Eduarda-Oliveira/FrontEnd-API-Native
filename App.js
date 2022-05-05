f5c23c9e6164bf3f1bee8ee64e14cea90130ff96
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-05-05 11:17:49 -0300

    retira a TabBar da pagina de login

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { Login } from './src/screens/login/login_screen';
 import { Search } from './src/screens/search/search_screen';
 import { Index } from './src/screens/index/index_screen';
 import Icon from 'react-native-vector-icons/Feather';
-import { getFocusedRouteNameFromRoute, NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 const screenOptions = (route, color) => {
   let iconName;
@@ -26,18 +28,36 @@ const screenOptions = (route, color) => {
   return <Icon name={iconName} color={color} size={28} />;
 };
 
-export default function App() {
-  
+ function Tabs() {  
   return (
-    <NavigationContainer>
+    <NavigationContainer independent={true}>
       <tab.Navigator
       screenOptions={({route}) => ({
         tabBarIcon: ({color}) => screenOptions(route, color),
       })}> 
-      <tab.Screen name="Login" component={Login} options={{header: () => null}}/>
-      <tab.Screen name="Search" component={Search} options={{header: () => null}}/>
       <tab.Screen name="Index" component={Index} options={{header: () => null}}/>
+      <tab.Screen name="Search" component={Search} options={{header: () => null}}/>
     </tab.Navigator>
     </NavigationContainer>
   );
 }
+function Routes() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{header: () => null}}
+        />
+        <Stack.Screen
+          name="Index"
+          component={Tabs}
+          options={{header: () => null}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
