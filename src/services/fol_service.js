3df3f326b2b1994cd609965b3ce07e0d1fc5fe2a
Gabriel Souza <azevedoedit@gmail.com>
2022-05-12 20:49:44 -0300

    add fol_service

---
+++ src/services/fol_service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const host = '127.0.0.1:8080'
+const modelRoute = `${host}/fol`
+
+const FolService = {
+  findAll: async function () {
+    let resp, msg;
+    await axios.get(`${modelRoute}/findAll`).then(
+      ({data}) => {
+      if (!data){
+        msg = {error: "Não há fols para exibir"}
+        resp = {msg}
+      }
+      if (data.length > 0) {
+        resp = {data}
+      } 
+    }
+  ).catch(
+    (e => {
+      console.log(`Deu ruim: ${e}`)
+    })
+  )
+  return resp
+  }
+}
+
+export default FolService;
