2f3aeea005247eca91559a97f9a5b8486a5accf6
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-05-14 15:18:16 -0300

    fix position elements

--- src/screens/search/search_screen.js
+++ src/screens/search/search_screen.js
@@ -1,98 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, ScrollView, View, KeyboardAvoidingView, TextInput, TouchableOpacity, FlatList, StatusBar, SafeAreaView} from 'react-native';
+import { StyleSheet, ScrollView, View, KeyboardAvoidingView, TextInput, TouchableOpacity, FlatList} from 'react-native';
 import { Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { FolService } from '../../services'
 
 export function Search({ navigation }) {
-  // const [documents, setDocuments] = useState([]);
+   const [documents, setDocuments] = useState([]);
   
   useEffect(()=>{
     async function getDocuments(){
       let fols = await FolService.findAll()
-      console.log(fols)
+      setDocuments(fols)
     }
     getDocuments()
   })
-  
   return (
-    <ScrollView style={styles.scrollView}>
-      <View>
-        <LinearGradient
-        // Background Linear Gradient
-        colors={['rgba(32, 70, 219, 0.76)', 'rgba(32, 129, 219, 0)']}
-        style={styles.background}
-        />
-        <TextInput
-        style={styles.input}
-        placeholder="Parameter"
-        autoCorrect={false}
-        />
+    <KeyboardAvoidingView style={styles.background}>
+      <LinearGradient
+          colors={['rgba(32, 70, 219, 0.76) 60%', 'rgba(32, 129, 219, 0)']}
+          style={styles.background}
+          />
+      <ScrollView>
+        <View style={styles.container}>
+          <TextInput
+          style={styles.input}
+          placeholder="Parameter"
+          autoCorrect={false}
+          />
           <Button
-        style={styles.btnSubmit}
-        title="Search"
-        ></Button>
-        {/* <FlatList
-          data={[
-            { key: "teste 1" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-            { key: "teste 2" },
-          ]}
-          renderItem={({item}) => <text>{item.key}</text>}
-        /> */}
-      </View>
-    </ScrollView>
+          style={styles.btnSubmit}
+          title="Search"
+          ></Button>
+      
+
+          {/* <FlatList
+            data={[
+              { key: "teste 1" },
+              { key: "teste 2" },
+              { key: "teste 2" },
+              { key: "teste 2" },
+              { key: "teste 2" },
+            ]}
+            renderItem={({item}) => <text>{item.key}</text>}
+          /> */}
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 const styles = StyleSheet.create({
@@ -103,8 +56,10 @@ const styles = StyleSheet.create({
     top: 0,
     height: 420,
   },
+
   container:{
     flex:1,
+    top: 70,
     alignItems: 'center',
     justifyContent: 'center',
     width: '90%',
@@ -118,7 +73,6 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 17,
     borderRadius: 15,
-    // stroke: 'black',
     padding: 10,
   },
 
@@ -135,7 +89,5 @@ const styles = StyleSheet.create({
     fontSize:18,
     color: '#FFF',
   },
-  scrollView: {
-    marginHorizontal: 20,
-  }
+ 
 });
