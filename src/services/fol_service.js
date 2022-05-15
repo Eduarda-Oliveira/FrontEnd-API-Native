0a997151555a66288f2914aa343ff94aa5dd529a
Eduarda-Oliveira <duda.oliveira.1503@gmail.com>
2022-05-15 15:02:49 -0300

    add route findByKeyword

--- src/services/fol_service.js
+++ src/services/fol_service.js
@@ -22,6 +22,26 @@ const FolService = {
     })
   )
   return resp
+  },
+
+  findByKeyword: async function () {
+    let resp, msg;
+    await axios.get(`${modelRoute}/findByKeyword`).then(
+      ({data}) => {
+      if (!data){
+        msg = {error: "Não há fols para exibir"}
+        resp = {msg}
+      }
+      if (data.length > 0) {
+        resp = {data}
+      } 
+    }
+  ).catch(
+    (e => {
+      console.log(`Deu ruim: ${e}`)
+    })
+  )
+  return resp 
   }
 }
 
